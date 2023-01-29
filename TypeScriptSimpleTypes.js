ce757270d20d6652bd99cb28baa91913cb1f39b6
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2023-01-29 16:20:28 +0530

    type script simple types

---
+++ TypeScriptSimpleTypes.js
@@ -0,0 +1,7 @@
+//Three primitive data types in Typescript:-
+var firstname = "sanidhya";
+var age = 20;
+var yesorno = false;
+console.log(firstname);
+console.log(age);
+console.log(yesorno);
---
+++ TypeScriptSimpleTypes.ts
@@ -0,0 +1,8 @@
+//Three primitive data types in Typescript:-
+
+let firstname:string="sanidhya";
+let age:number=20;
+let yesorno:boolean=false;
+console.log(firstname);
+console.log(age);
+console.log(yesorno);
